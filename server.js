f9db82c1a91e9babe6a795bb750cea6f7f65390a
System <none>
2017-09-07 03:55:13 +0000

    Checkpoint

---
+++ server.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var app = express();
+app.set('view engine', 'pug')
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+
+app.get('/', function (req, res) {
+  res.render('index', { title: 'Hey', message: 'Hello there!' })
+})
+
+// listen for requests :)
+var listener = app.listen(process.env.PORT, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});

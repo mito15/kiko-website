31901fc45be24800b7a162c6d2e82440a410643a
Glitch (pug) <none>
2019-09-11 05:04:36 +0000

    🏘📻 Checkpoint ./server.js:5770466/145 ./views/layout.pug:5770466/1094

--- server.js
+++ server.js
@@ -11,31 +11,27 @@ app.get('/', function (req, res) {
   res.render('index', { title: '屋外照明・屋内照明・LED照明・光学薄膜技術の横浜機工' });
 });
 
-app.get('/index.html', function (req, res) {
-  res.render('index', { title: '屋外照明・屋内照明・LED照明・光学薄膜技術の横浜機工' });
-});
-
-app.get('/company.html', function (req, res) {
+app.get('/company', function (req, res) {
   res.render('company', { title: '会社概要|屋外照明・屋内照明・LED照明・光学薄膜技術の横浜機工' });
 });
 
-app.get('/gallery.html', function (req, res) {
+app.get('/gallery', function (req, res) {
   res.render('gallery', { title: '製造設備と技術|屋外照明・屋内照明・LED照明・光学薄膜技術の横浜機工' });
 });
 
-app.get('/contact.html', function (req, res) {
+app.get('/contact', function (req, res) {
   res.render('contact', { title: 'お問い合わせ|屋外照明・屋内照明・LED照明・光学薄膜技術の横浜機工' });
 });
 
-app.get('/quality.html', function (req, res) {
+app.get('/quality', function (req, res) {
   res.render('quality', { title: '品質・環境|屋外照明・屋内照明・LED照明・光学薄膜技術の横浜機工' });
 });
 
-app.get('/works.html', function (req, res) {
+app.get('/works', function (req, res) {
   res.render('works', { title: '事業紹介|屋外照明・屋内照明・LED照明・光学薄膜技術の横浜機工' });
 });
 
-app.get('/aboutus.html', function (req, res) {
+app.get('/aboutus', function (req, res) {
   res.render('aboutus', { title: '私たちの活動|屋外照明・屋内照明・LED照明・光学薄膜技術の横浜機工' });
 });
 
